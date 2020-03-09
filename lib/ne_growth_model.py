ee8162a700a93984716ef827e800fe0377c7559e
Will Haslett <>
2020-03-09 18:22:21 -0400

    Fixed group_by

--- lib/ne_growth_model.py
+++ lib/ne_growth_model.py
@@ -5,12 +5,12 @@ from scipy.optimize import least_squares
 
 # Local
 from etl import df_regional
+from etl import df_us
 from models import ggrowth
 
 pp = pprint.pprint
 
-df = df_regional['NE'].groupby('date').sum().reset_index()
+df = df_regional['NE'].groupby('int_date').sum().reset_index()
 df = df[['int_date', 'cases']]
-
 
 pp(df)
