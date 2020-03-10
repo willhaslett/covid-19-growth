12b1b5e8fd1c6ba028215a6eb6dd3412fd48b95a
Will Haslett <>
2020-03-10 12:01:41 -0400

    Model coefficients still jacked

--- lib/models.py
+++ lib/models.py
@@ -2,15 +2,15 @@ import numpy as np
 
 # Generalized growth model https://www.sciencedirect.com/science/article/pii/S1755436516000037
 # TODO: Get this working correctly
-def ggrowth(date, r, m):
+def ggrowth(day, r, m):
     # Presume cases(0) = 0
     c0 = 200
     a = c0 ** (1/m) 
-    return ((r/m) * date + a) ** m
+    return ((r/m) * day + a) ** m
 
 # Exponential
-def egrowth(date, x):
-    return np.e ** x
+def egrowth(day, a, x):
+    return (a * (np.e ** (day * x)))
 
 
 # TODO: Logistic model
