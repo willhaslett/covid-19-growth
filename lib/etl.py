012bddf232345ea270667359c09811fe6e5cd8a7
Will Haslett <>
2020-03-10 21:03:44 -0400

    Notebook work

--- lib/etl.py
+++ lib/etl.py
@@ -1,10 +1,6 @@
 import pandas as pd
 from operator import itemgetter
 
-# For now, just working with the confirmed cases CSV
-# This file is intended to only setup the initial dataframe, with optional regional filtering. Import the dataframe to your downstream files as:
-#     from etl import df_us # and/or df_regional
-
 # TODO: the other regions
 REGIONS = {
     'NE': ['ME', 'NY', 'NJ', 'VT', 'MA', 'RI', 'CT', 'NH', 'PA'],
@@ -12,7 +8,6 @@ REGIONS = {
 }
 
 # df_all: All raw data converted to long format with dates as Pandas timestamps
-#####################################################################################
 df_all = pd.read_csv('confirmed_cases.csv')
 date_cols = df_all.filter(regex=('^\d+/\d+/\d+$')).columns.array
 df_all = pd.melt(df_all, id_vars=['Province/State', 'Country/Region', 'Lat',
@@ -21,7 +16,6 @@ df_all.date = pd.to_datetime(df_all.date, format='%m/%d/%y')
 df_all['day'] = df_all['days_since'] = (df_all.date - pd.to_datetime(df_all.date.iloc[0])).astype('timedelta64[D]')
 
 # df_us: Filter out non-US, rename columns, split state into column
-#####################################################################################
 df_us = df_all[df_all['Country/Region'] == 'US'].reset_index()
 df_us = df_us.drop(columns='Country/Region')
 df_us = df_us.rename(
@@ -30,7 +24,6 @@ df_us.city_county, df_us['state'] = itemgetter(
     0, 1)(df_us.city_county.str.split(', ').str)
 
 # df_regional: A dict of US dataframes by region
-#####################################################################################
 def for_region(state_list):
     return df_us[df_us.state.isin(state_list)]
 
