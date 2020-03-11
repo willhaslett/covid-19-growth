11fd2a2c6a613a90383cb1636d82fb8b3c559f6d
Will Haslett <>
2020-03-10 22:55:11 -0400

    Leading underscore on intermediate objects

--- lib/etl.py
+++ lib/etl.py
@@ -8,20 +8,22 @@ from operator import itemgetter
 #   sum_by_date()
 
 # df_all
-df_all = pd.read_csv('confirmed_cases.csv')
-_date_cols = df_all.filter(regex=('^\d+/\d+/\d+$')).columns.array
-df_all = pd.melt(df_all, id_vars=['Province/State', 'Country/Region', 'Lat',
-                                  'Long'], value_vars=date_cols, var_name='date', value_name='cases')
-df_all.date = pd.to_datetime(df_all.date, format='%m/%d/%y')
-df_all['day'] = df_all['days_since'] = (df_all.date - pd.to_datetime(df_all.date.iloc[0])).astype('timedelta64[D]')
+_df_all = pd.read_csv('confirmed_cases.csv')
+_date_cols = _df_all.filter(regex=('^\d+/\d+/\d+$')).columns.array
+_df_all = pd.melt(_df_all, id_vars=['Province/State', 'Country/Region', 'Lat',
+                                  'Long'], value_vars=_date_cols, var_name='date', value_name='cases')
+_df_all.date = pd.to_datetime(_df_all.date, format='%m/%d/%y')
+_df_all['day'] = _df_all['days_since'] = (_df_all.date - pd.to_datetime(_df_all.date.iloc[0])).astype('timedelta64[D]')
+df_all = _df_all
 
 # df_us
-df_us = df_all[df_all['Country/Region'] == 'US'].reset_index()
-df_us = df_us.drop(columns='Country/Region')
-df_us = df_us.rename(
+_df_us = df_all[df_all['Country/Region'] == 'US'].reset_index()
+_df_us = _df_us.drop(columns='Country/Region')
+_df_us = _df_us.rename(
     columns={'Province/State': 'city_county', 'Lat': 'lat', 'Long': 'long'})
-df_us.city_county, df_us['state'] = itemgetter(
-    0, 1)(df_us.city_county.str.split(', ').str)
+_df_us.city_county, _df_us['state'] = itemgetter(
+    0, 1)(_df_us.city_county.str.split(', ').str)
+df_us = _df_us
 
 def for_states(state_list):
     return df_us[df_us.state.isin(state_list)].reset_index()
