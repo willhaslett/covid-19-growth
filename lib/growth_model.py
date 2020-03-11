c89ebcb754408224728c2e5e2adf9d6805085b0c
Will Haslett <>
2020-03-11 14:50:12 -0400

    Working getting lmfit going

--- lib/growth_model.py
+++ lib/growth_model.py
@@ -1,44 +1,23 @@
 from pprint import pprint as pp
 import pandas as pd
 from scipy.optimize import curve_fit
+from lmfit import Model
+from constants import REGIONS as regions
 from etl import df_us
 import models
-from constants import REGIONS as regions
 
-
-# from lmfit import minimize, Parameters
-# def residual(params, x, data, eps_data):
-#     amp = params['amp']
-#     phaseshift = params['phase']
-#     freq = params['frequency']
-#     decay = params['decay']
-#     model = amp * sin(x*freq + phaseshift) * exp(-x*x*decay)
-#     return (data-model) / eps_data
-# params = Parameters()
-# params.add('amp', value=10)
-# params.add('decay', value=0.007)
-# params.add('phase', value=0.2)
-# params.add('frequency', value=3.0)
-# out = minimize(residual, params, args=(x, data, eps_data))
-
-# Choose model
-model = models.lgrowth
-
-# Starting with the Northeast region
-REGION = 'NE'
-_df = df_us.groupby('day').sum().reset_index()
+# Aggregated US data
+_df = df_us.groupby('day').sum().reset_index().to_numpy()
 _df = _df[['day', 'cases']]
 
-# Scipy wants Numpy arrays
-_np_array = _df.to_numpy()
-_day = _np_array[:, 0]
-_cases = _np_array[:, 1]
-
-# Fit model
-_model_parameters, _model_covariance = curve_fit(model, _day, _cases)
-
-# Predicted case counts
-pred_cases = _df.drop(['cases'], axis=1).applymap(
-    lambda x: model(x, _model_parameters[0]))
-
-pp(pred_cases)
+# Choose model
+model = models.lmodel
+print()
+print('parameter names: {}'.format(model.param_names))
+print('independent variables: {}'.format(model.independent_vars))
+print()
+params = model.make_params()
+pp(params)
+print()
+result = model.fit(_df, params)
+pp(result)
--- lib/models.py
+++ lib/models.py
@@ -1,5 +1,15 @@
 import numpy as np
-from lmfit import minimize, Parameters
+from lmfit import Model
+
+# Linear
+def lgrowth(day, m):
+    return (m * day)
+lmodel = Model(lgrowth)
+
+# Exponential
+def egrowth(day, a, x):
+    return (a * (np.e ** (day * x)))
+emodel = Model(egrowth)
 
 # Generalized growth https://www.sciencedirect.com/science/article/pii/S1755436516000037
 # TODO: Get this working correctly
@@ -8,11 +18,4 @@ def ggrowth(day, r, m):
     c0 = 200
     a = c0 ** (1/m) 
     return ((r/m) * day + a) ** m
-
-# Exponential
-def egrowth(day, a, x):
-    return (a * (np.e ** (day * x)))
-
-# Linear
-def lgrowth(day, m):
-    return (m * day)
+gmodel = Model(ggrowth)
