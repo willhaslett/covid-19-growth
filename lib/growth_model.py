a59aeab2eaf79ef0ca898a1e887a9f7ea77911db
Will Haslett <>
2020-03-11 15:13:35 -0400

    lmfit.fit still not working

--- lib/growth_model.py
+++ lib/growth_model.py
@@ -7,17 +7,20 @@ from etl import df_us
 import models
 
 # Aggregated US data
-_df = df_us.groupby('day').sum().reset_index().to_numpy()
+_df = df_us.groupby('day').sum().reset_index()
 _df = _df[['day', 'cases']]
+pp(_df)
+pp('')
 
-# Choose model
 model = models.lmodel
+pp(model)
+pp('parameter names: {}'.format(model.param_names))
+pp('independent variables: {}'.format(model.independent_vars))
+pp('')
+
+params = model.make_params(m=1)
+print(params)
 print()
-print('parameter names: {}'.format(model.param_names))
-print('independent variables: {}'.format(model.independent_vars))
-print()
-params = model.make_params()
-pp(params)
-print()
+
 result = model.fit(_df, params)
 pp(result)
