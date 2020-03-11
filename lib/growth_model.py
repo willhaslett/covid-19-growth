e1851d49dc9bf90514d722dd640b04e77f0cea1c
Will Haslett <>
2020-03-11 16:16:58 -0400

    Plotting with model fit works

--- lib/growth_model.py
+++ lib/growth_model.py
@@ -1,8 +1,7 @@
 from pprint import pprint as pp
 import pandas as pd
-from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
 from lmfit import Model
-from constants import REGIONS as regions
 from etl import df_us
 import models
 
@@ -11,15 +10,19 @@ _df = df_us.groupby('day').sum().reset_index()
 _df = _df[['day', 'cases']]
 pp(hasattr(_df, '__array__'))
 
+
+# Pick a model. See models.py
 model = models.lmodel
 pp(model)
 pp('parameter names: {}'.format(model.param_names))
 pp('independent variables: {}'.format(model.independent_vars))
 pp('')
 
-params = model.make_params(m=1)
-print(params)
-print()
-
 result = model.fit(_df.cases, day=_df.day, m=1)
-pp(result)
+print(result.fit_report())
+
+plt.plot(_df.day, _df.cases, 'bo')
+plt.plot(_df, result.init_fit, 'k--', label='initial fit')
+plt.plot(_df, result.best_fit, 'r-', label='best fit')
+plt.legend(loc='best')
+plt.show()
