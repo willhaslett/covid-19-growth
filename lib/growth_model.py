f9e4e53f56c32ef4b60df231552a5bbb6dcc727c
Will Haslett <>
2020-03-11 15:46:21 -0400

    lmfit runs

--- lib/growth_model.py
+++ lib/growth_model.py
@@ -9,8 +9,7 @@ import models
 # Aggregated US data
 _df = df_us.groupby('day').sum().reset_index()
 _df = _df[['day', 'cases']]
-pp(_df)
-pp('')
+pp(hasattr(_df, '__array__'))
 
 model = models.lmodel
 pp(model)
@@ -22,5 +21,5 @@ params = model.make_params(m=1)
 print(params)
 print()
 
-result = model.fit(_df, params)
+result = model.fit(_df.cases, day=_df.day, m=1)
 pp(result)
