aae0d1afdfe770089ec5bc62451e25700f545f5c
Will Haslett <>
2020-03-11 18:37:33 -0400

    Linear model works

--- lib/growth_model.py
+++ lib/growth_model.py
@@ -4,25 +4,10 @@ import matplotlib.pyplot as plt
 from lmfit import Model
 from etl import df_us
 import models
+from lmfit.models import LinearModel
 
-# Aggregated US data
 _df = df_us.groupby('day').sum().reset_index()
-_df = _df[['day', 'cases']]
-pp(hasattr(_df, '__array__'))
-
-
-# Pick a model. See models.py
-model = models.lmodel
-pp(model)
-pp('parameter names: {}'.format(model.param_names))
-pp('independent variables: {}'.format(model.independent_vars))
-pp('')
-
-result = model.fit(_df.cases, day=_df.day, m=1)
+model = LinearModel()
+params = model.make_params()
+result = model.fit(_df.day, params, x=_df.day.to_list())
 print(result.fit_report())
-
-plt.plot(_df.day, _df.cases, 'bo')
-plt.plot(_df, result.init_fit, 'k--', label='initial fit')
-plt.plot(_df, result.best_fit, 'r-', label='best fit')
-plt.legend(loc='best')
-plt.show()
--- lib/models.py
+++ lib/models.py
@@ -1,21 +1,42 @@
 import numpy as np
-from lmfit import Model
+from lmfit import Model, models
 
-# Linear
-def lgrowth(day, m):
-    return (m * day)
-lmodel = Model(lgrowth)
+def linear(x, slope=1.0, intercept=0.0):
+    """Return a linear function.
 
-# Exponential
-def egrowth(day, a, x):
-    return (a * (np.e ** (day * x)))
-emodel = Model(egrowth)
+    x -> slope * x + intercept
 
-# Generalized growth https://www.sciencedirect.com/science/article/pii/S1755436516000037
-# TODO: Get this working correctly
-def ggrowth(day, r, m):
-    # Presume cases(0) = 0
-    c0 = 200
-    a = c0 ** (1/m) 
-    return ((r/m) * day + a) ** m
-gmodel = Model(ggrowth)
+    """
+    return slope * x + intercept
+
+class LinearModel(Model):
+    """Linear model, with two Parameters ``intercept`` and ``slope``.
+
+    Defined as:
+
+    .. math::
+
+        f(x; m, b) = m x + b
+
+    with  ``slope`` for :math:`m` and  ``intercept`` for :math:`b`.
+
+    """
+
+    def __init__(self, independent_vars=['x'], prefix='', nan_policy='raise',
+                 **kwargs):
+        kwargs.update({'prefix': prefix, 'nan_policy': nan_policy,
+                       'independent_vars': independent_vars})
+        super().__init__(linear, **kwargs)
+
+    def guess(self, data, x=None, **kwargs):
+        """Estimate initial model parameter values from data."""
+        sval, oval = 0., 0.
+        if x is not None:
+            sval, oval = np.polyfit(x, data, 1)
+        pars = self.make_params(intercept=oval, slope=sval)
+        return models.update_param_vals(pars, self.prefix, **kwargs)
+
+# def ggrowth(day, r, m, c0):
+#     # Generalized growth https://www.sciencedirect.com/science/article/pii/S1755436516000037
+#     a = c0 ** (1/m)
+#     return ((r/m) * day + a) ** m
