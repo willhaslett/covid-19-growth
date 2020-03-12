f82b7249d40b966e592ec5eeeed5f19756845619
Will Haslett <>
2020-03-11 22:52:27 -0400

    Working on growth model NaN problem

--- lib/growth_model.py
+++ lib/growth_model.py
@@ -3,10 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from lmfit import Model
 from etl import df_us
-from models import LinearModel
+from models import LinearModel, GeneralizedGrowthModel  
 
 _df = df_us.groupby('day').sum().reset_index()
-model = LinearModel()
+model = GeneralizedGrowthModel()
 params = model.make_params()
 result = model.fit(_df.day, params, x=_df.day.to_list())
 print(result.fit_report())
--- lib/models.py
+++ lib/models.py
@@ -1,5 +1,5 @@
 import numpy as np
-from lmfit import Model
+from lmfit import Model, models
 
 def linear(x, slope=1.0, intercept=0.0):
     return slope * x + intercept
@@ -11,22 +11,18 @@ class LinearModel(Model):
                        'independent_vars': independent_vars})
         super().__init__(linear, **kwargs)
 
-    def guess(self, data, x=None, **kwargs):
-        """Estimate initial model parameter values from data."""
-        sval, oval = 0., 0.
-        if x is not None:
-            sval, oval = np.polyfit(x, data, 1)
-        pars = self.make_params(intercept=oval, slope=sval)
-        return models.update_param_vals(pars, self.prefix, **kwargs)
-
-def ggrowth(x, r, m, c0):
+def ggrowth(x, r, m):
     # Generalized growth https://www.sciencedirect.com/science/article/pii/S1755436516000037
-    a = c0 ** (1/m)
-    return ((r/m) * x + a) ** m
+    return ((r/m) * x) ** m
+
+# def ggrowth(x, r, m, c0):
+#     # Generalized growth https://www.sciencedirect.com/science/article/pii/S1755436516000037
+#     a = c0 ** (1/m)
+#     return ((r/m) * x + a) ** m
 
 class GeneralizedGrowthModel(Model):
     def __init__(self, independent_vars=['x'], prefix='', nan_policy='raise',
                  **kwargs):
-        kwargs.update({'prefix': prefix, 'nan_policy': nan_policy,
+        kwargs.update({'prefix': prefix, 'nan_policy': 'raise',
                        'independent_vars': independent_vars})
         super().__init__(ggrowth, **kwargs)
