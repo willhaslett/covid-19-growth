b4d6df21bc775f023215dd6dc1e9f983d187022d
Will Haslett <>
2020-03-13 16:55:55 -0400

    Factoed out df creation function. Updated tests.

--- lib/etl.py
+++ lib/etl.py
@@ -8,47 +8,32 @@ import constants
 #   for_province_state()
 #   sum_by_date()
 
-# df_cases
-_df_cases = pd.read_csv('confirmed_cases.csv')
-_df_cases = _df_cases.rename(columns=constants.RENAMED_COLUMNS)
-_date_cols = _df_cases.filter(regex=('^\d+/\d+/\d+$')).columns.array
-_df_cases = pd.melt(_df_cases, id_vars=['province_state', 'country', 'lat',
-                                  'long'], value_vars=_date_cols, var_name='date', value_name='cases')
-_df_cases.date = pd.to_datetime(_df_cases.date, format='%m/%d/%y')
-_df_cases['day'] = (_df_cases.date - pd.to_datetime(_df_cases.date.iloc[0])).astype('timedelta64[D]')
-df_cases = _df_cases
+# Perform ETL on a COVID-19 CSV file
+def df_from_csv(file_name):
+    _df = pd.read_csv(file_name)
+    _df = _df.rename(columns=constants.RENAMED_COLUMNS)
+    _date_cols = _df.filter(regex=('^\d+/\d+/\d+$')).columns.array
+    _df = pd.melt(_df, id_vars=['province_state', 'country', 'lat',
+                                      'long'], value_vars=_date_cols, var_name='date', value_name='cases')
+    _df.date = pd.to_datetime(_df.date, format='%m/%d/%y')
+    _df['day'] = (_df.date - pd.to_datetime(_df.date.iloc[0])).astype('timedelta64[D]')
+    return _df
 
-# df_deaths
-_df_deaths = pd.read_csv('confirmed_cases.csv')
-_df_deaths = _df_deaths.rename(columns=constants.RENAMED_COLUMNS)
-_date_cols = _df_deaths.filter(regex=('^\d+/\d+/\d+$')).columns.array
-_df_deaths = pd.melt(_df_deaths, id_vars=['province_state', 'country', 'lat',
-                                  'long'], value_vars=_date_cols, var_name='date', value_name='cases')
-_df_deaths.date = pd.to_datetime(_df_deaths.date, format='%m/%d/%y')
-_df_deaths['day'] = (_df_deaths.date - pd.to_datetime(_df_deaths.date.iloc[0])).astype('timedelta64[D]')
-df_deaths = _df_deaths
-
-# df_recovered
-_df_recovered = pd.read_csv('confirmed_cases.csv')
-_df_recovered = _df_recovered.rename(columns=constants.RENAMED_COLUMNS)
-_date_cols = _df_recovered.filter(regex=('^\d+/\d+/\d+$')).columns.array
-_df_recovered = pd.melt(_df_recovered, id_vars=['province_state', 'country', 'lat',
-                                  'long'], value_vars=_date_cols, var_name='date', value_name='cases')
-_df_recovered.date = pd.to_datetime(_df_recovered.date, format='%m/%d/%y')
-_df_recovered['day'] = (_df_recovered.date - pd.to_datetime(_df_recovered.date.iloc[0])).astype('timedelta64[D]')
-df_recovered = _df_recovered
+df_cases = df_from_csv('confirmed_cases.csv')
+df_deaths = df_from_csv('deaths.csv')
+df_recovered = df_from_csv('recovered_cases.csv')
 
 # General purpose filter.
-def filter(column, value):
-    return df_all[df_all[column] == value].reset_index()
+def filter(df, column, value):
+    return df[df[column] == value].reset_index()
 
 # Filter on country
-def for_country(country):
-    return filter('country', country)
+def for_country(df, country):
+    return filter(df, 'country', country)
 
 # Filter on country province_state
-def for_province_state(province_state):
-    return filter('province_state', province_state)
+def for_province_state(df, province_state):
+    return filter(df, 'province_state', province_state)
 
 # Split province_state on the string ', ' creating a new column 'state'
 def state_to_col_destructive(df):
@@ -59,5 +44,3 @@ def state_to_col_destructive(df):
 # Return input with all rows collapsed by date and cases summed
 def sum_by_date(df):
     return df.groupby('date').sum().reset_index()
-
-# Returns a dataframe summary
--- lib/tests.py
+++ lib/tests.py
@@ -2,19 +2,19 @@ import pandas as pd
 from pprint import pprint as pp
 import etl
 
-print("\nCases:")
+print("\nCases (head):")
 pp(etl.df_cases.head())
 
-print("\nDeaths:")
+print("\nDeaths (head):")
 pp(etl.df_deaths.head())
 
-print("\nRecovered:")
+print("\nRecovered (head):")
 pp(etl.df_recovered.head())
 
-# print("\nCases for one country:")
-# pp(etl.for_country('France').head())
-# 
-# print("\nCases for one province_state:")
-# pp(etl.for_province_state('British Columbia').head())
-# 
-# print("\nTests passed")
+print("\nCases for one country (head):")
+pp(etl.for_country(etl.df_cases, 'France').head())
+
+print("\nCases for one province_state (head):")
+pp(etl.for_province_state(etl.df_cases, 'British Columbia').head())
+
+print("\nTests passed")
