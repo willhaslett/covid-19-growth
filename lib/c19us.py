7870dd8034850cafc7aa7dc126fb07ecd14c3598
Will Haslett <>
2020-03-14 18:47:51 -0400

    Reorganized c19us.py

--- lib/c19us.py
+++ lib/c19us.py
@@ -11,34 +11,21 @@ from pprint import pprint as pp
 # `df_us_population` 2019 US census population data by state, sub-region, and region
 
 # Functions
-# `us_data(df)` Filter input dataframe on US rows.
-# `us_data_state(df)` Filter input US dataframe state-level records.
+# `us_data(df)` Filter input dataframe on US rows
+# `us_data_state(df)` Filter input US dataframe state-level records
 # `population_for_state(state_name)`
 
-# All US data. Applied to cases, deaths, and recoveries in the dictionary `df_us`
+# US population by state, region and subregion
+df_us_population = pd.read_csv('csv/us_population.csv')
+
+# All US data. Used to build `df_us`, which should not be used unless you want to preserve the
+# mixed types in the province_state column of the JH US data
 def us_data(df):
     df = df.rename(columns={'province_state': 'state'})
     # For consistency with census data
     df.state = df.state.apply(lambda state: (state, 'District of Columbia') [state == 'Washington, D.C.'])
     df.state, df['state_abbrev'] = itemgetter(0, 1)(df.state.str.split(', ').str)
     return df
-
-# Sate-level US data. Applied to cases, deaths, and recoveries in the dictionary `df_us_states`
-def us_data_state(df):
-    df = df[df['state'].isin(df_us_population['state'])]
-    return df[['day', 'state', 'cases']]
-
-# County-level US data. Applied to cases, deaths, and recoveries in the dictionary `df_us_states`
-def us_data_county(df):
-    df = df[~df['state'].isin(df_us_population['state'])]
-    return df[['day', 'state', 'cases']]
-
-# Unused at present
-def population_for_state(state_name):
-    return df_us_population[df_us_population.state == state_name].iloc[0].population_2019
-
-# US population
-df_us_population = pd.read_csv('csv/us_population.csv')
 
 # Dict of all US data
 df_us = {
@@ -47,6 +34,11 @@ df_us = {
     'recovered': us_data(c19all.for_country(c19all.df_recovered, 'US'))
 }
 
+# State-level US data. Used to build `df_us_state`
+def us_data_state(df):
+    df = df[df['state'].isin(df_us_population['state'])]
+    return df[['day', 'state', 'cases']]
+
 # Dict of US state-level data
 df_us_state = {
     'cases': us_data_state(df_us['cases']),
@@ -54,9 +46,21 @@ df_us_state = {
     'recovered': us_data_state(df_us['recovered']),
 }
 
+# County-level US data. Used to build `df_us_county`
+def us_data_county(df):
+    df = df[~df['state'].isin(df_us_population['state'])]
+    return df[['day', 'state', 'cases']]
+
 # Dict of US county-level data
 df_us_county = {
     'cases': us_data_county(df_us['cases']),
     'deaths': us_data_county(df_us['deaths']),
     'recovered': us_data_county(df_us['recovered']),
 }
+
+# Region-level US data. Used to build `df_us_region`
+def us_data_region(df):
+    regions = df_us_population.region.unique().tolist()
+    pp(regions)
+
+us_data_region(df_us_state['cases'])
