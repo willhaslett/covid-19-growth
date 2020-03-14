24d222ef9fc10d7417a19c30792b8697dfd4c791
Will Haslett <>
2020-03-13 22:26:06 -0400

    Moved us file to us folder. Added us population CSV.

---
+++ lib/united_states/state_populations.py
@@ -0,0 +1,51 @@
+state,state_abbrev,population_2019,sub_region,region
+Massachusetts,MA,6892503,new_england,northeast
+Connecticut,CT,3565287,new_england,northeast
+New Hampshire,NH,1359711,new_england,northeast
+Maine,ME,1344212,new_england,northeast
+Rhode Island,RI,1059361,new_england,northeast
+Vermont,VT,623989,new_england,northeast
+New York,NY,19453561,mid_atlantic,northeast
+Pennsylvania,PE,12801989,mid_atlantic,northeast
+New Jersey,NJ,8882190,mid_atlantic,northeast
+Florida,FL,21477737,south_atlantic,south
+Georgia,GA,10617423,south_atlantic,south
+North Carolina,NC,10488084,south_atlantic,south
+Virginia,VA,8535519,south_atlantic,south
+Maryland,MY,6045680,south_atlantic,south
+South Carolina,SC,5148714,south_atlantic,south
+West Virginia,WV,1792147,south_atlantic,south
+Delaware,DE,973764,south_atlantic,south
+District of Columbia,DC,705749,south_atlantic,south
+Alabama,AL,4903185,east_south_central,south
+Kentucky,KY,4467673,east_south_central,south
+Mississippi,MS,2976149,east_south_central,south
+Texas,TX,28995881,west_south_central,south
+Louisiana,LA,4648794,west_south_central,south
+Oklahoma,OK,3956971,west_south_central,south
+Arkansas,AR,3017804,west_south_central,south
+Illinois,IL,12671821,east_north_central,midwest
+Ohio,OH,11689100,east_north_central,midwest
+Michigan,MI,9986857,east_north_central,midwest
+Indiana,IN,6732219,east_north_central,midwest
+Wisconsin,WI,5822434,east_north_central,midwest
+Missouri,MO,6137428,west_north_central,midwest
+Minnesota,MI,5639632,west_north_central,midwest
+Iowa,IA,3155070,west_north_central,midwest
+Kansas,KS,2913314,west_north_central,midwest
+Nebraska,NE,1934408,west_north_central,midwest
+South Dakota,SD,884659,west_north_central,midwest
+North Dakota,ND,762062,west_north_central,midwest
+Arizona,AZ,7278717,mountain,west
+Colorado,CO,5758736,mountain,west
+Utah,UT,3205958,mountain,west
+Nevada,NV,3080156,mountain,west
+New Mexico,NM,2096829,mountain,west
+Idaho,ID,1787065,mountain,west
+Montana,MT,1068778,mountain,west
+Wyoming,WY,578759,mountain,west
+California,CA,39512223,pacific,west
+Washington,WA,7614893,pacific,west
+Oregon,OR,4217737,pacific,west
+Hawaii,HI,1415872,pacific,west
+Alaska,AK,731545,pacific,west
