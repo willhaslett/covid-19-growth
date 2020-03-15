b6cb207f6270ef9c75ed2da72457888d6783d2ff
Will Haslett <>
2020-03-15 18:16:32 -0400

    Working on US dataframe

--- lib/c19us.py
+++ lib/c19us.py
@@ -24,13 +24,6 @@ def _us_data(df):
     df.state_county = df.state_county.apply(lambda state: (state, 'District of Columbia') [state == 'Washington, D.C.'])
     df = df.drop(['country'], axis=1)
     return df
-
-# Dict of all US data.
-df_us = {
-    'cases': _us_data(c19all.for_country(c19all.df_cases, 'US')),
-    'deaths': _us_data(c19all.for_country(c19all.df_deaths, 'US')),
-    'recovered': _us_data(c19all.for_country(c19all.df_recovered, 'US'))
-}
 print("\ndf_us:")
 print(df_us['cases'])
 
--- lib/df_us_new.py
+++ lib/df_us_new.py
@@ -1,9 +1,40 @@
 import pandas as pd
 import pickle
+import c19all
+import constants
+from constants import US_LOCATIONS_IN_SOURCE as locations
+from constants import US_POPULATION as population
+from constants import CRUISE_SHIPS as cruise_ships
 
-# df = pd.DataFrame(data=pickle.load(open('pickles/c19_us.p', 'rb'))['cases'])
-df = data=pickle.load(open('pickles/c19_us.p', 'rb'))['cases']
+output_columns = [
+    'date',
+    'day',
+    'cases',
+    'state',
+    'county',
+    'territory',
+    'is_state',
+    'lat',
+    'long'
+]
 
-places = pd.DataFrame(df.state_county.unique())
+def _us_data(df):
+    df = df.rename(columns={'province_state': '_location'})
+    df['state'] = None
+    # Remove cruise ship data
+    df = df[~df._location.isin(cruise_ships)]
+    # Rename Washington D.C. records
+    df.state_county = df._location.apply(lambda state: (state, 'District of Columbia') [state == 'Washington, D.C.'])
+    df = df.drop(['country'], axis=1)
+    return df
 
-places.to_csv('csv/foo.csv', index=False)
+df = _us_data(c19all.for_country(c19all.df_cases, 'US'))
+
+print(df)
+
+# Raw US data
+# df_us = {
+#     'cases': _us_data(c19all.for_country(c19all.df_cases, 'US')),
+#     'deaths': _us_data(c19all.for_country(c19all.df_deaths, 'US')),
+#     'recovered': _us_data(c19all.for_country(c19all.df_recovered, 'US'))
+# }
