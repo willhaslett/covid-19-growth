0465d44d416a01edb7fb19a28e19da9aa0538cd7
Will Haslett <>
2020-03-15 23:14:48 -0400

    Updated pickle creation

--- lib/update_pickles.py
+++ lib/update_pickles.py
@@ -2,13 +2,13 @@ import pickle
 import c19all
 import c19us 
 
-dfs = [
-    {'name': 'c19_all', 'df': c19all.df_all},
-    {'name': 'c19_us', 'df': c19us.df_us},
-    {'name': 'c19_us_state', 'df': c19us.df_us_state},
-    {'name': 'c19_us_county', 'df': c19us.df_us_county},
+_dicts = [
+    {'name': 'c19_all', 'dict': c19all.df_all},
+    {'name': 'c19_us', 'dict': c19us.df_us},
 ]
 
-for df_dict in dfs:
-    pickle_file = open(f'pickles/{df_dict["name"]}.p', 'wb')
-    pickle.dump(df_dict['df'], pickle_file)
+for _dict in _dicts:
+    pickle_file = open(f'pickles/{_dict["name"]}.p', 'wb')
+    pickle.dump(_dict['dict'], pickle_file)
+
+print('Updated pickle files')
