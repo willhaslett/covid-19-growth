cafc733f3d614406adaef97759951aaac29d4593
Will Haslett <>
2020-03-16 19:56:08 -0400

    Cleaned up tests.py

--- lib/tests.py
+++ lib/tests.py
@@ -1,27 +1,26 @@
-import pandas as pd
-from pprint import pprint as pp
 print('Takes a couple of minutes because the US data parser has a lot to do')
+import pandas as pd
 import c19all
 import c19us
 
 # c19all.py
 print("\nGlobal cases (head):")
-pp(c19all.df_all['cases'].head())
+print(c19all.df_all['cases'].head())
 print("\nGlobal deaths (head):")
-pp(c19all.df_all['deaths'].head())
+print(c19all.df_all['deaths'].head())
 print("\nGlobal recovered (head):")
-pp(c19all.df_all['recovered'].head())
+print(c19all.df_all['recovered'].head())
 print("\nCases for one country (head):")
-pp(c19all.for_country(c19all.df_all['cases'], 'France').head())
+print(c19all.for_country(c19all.df_all['cases'], 'France').head())
 print("\nCases for one province_state (head):")
-pp(c19all.for_province_state(c19all.df_all['cases'], 'British Columbia').head())
+print(c19all.for_province_state(c19all.df_all['cases'], 'British Columbia').head())
 
 # c19us.py
 print("\nUS Cases (head):")
-pp(c19us.df_us['cases'].head())
+print(c19us.df_us['cases'].head())
 print("\nUS Deaths (head):")
-pp(c19us.df_us['deaths'].head())
+print(c19us.df_us['deaths'].head())
 print("\nUS Recovered (head):")
-pp(c19us.df_us['recovered'].head())
+print(c19us.df_us['recovered'].head())
 
 print("\nTests passed")
