4900c64ff6b5e3589a83f91b186778a419b1c93c
Will Haslett <>
2020-03-16 00:04:14 -0400

    Updated readme

--- lib/c19us.py
+++ lib/c19us.py
@@ -104,3 +104,5 @@ df_us = {
     'deaths': _us_data(c19all.for_country(c19all.df_deaths, 'US')),
     'recovered': _us_data(c19all.for_country(c19all.df_recovered, 'US'))
 }
+
+print(df_us['cases'])
