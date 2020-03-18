65ad6e2220fbac1085bca5a0cc2178655183d155
Will Haslett <>
2020-03-18 09:04:21 -0400

    Fixed some not-pythonic comments

--- lib/c19all.py
+++ lib/c19all.py
@@ -3,21 +3,23 @@ from operator import itemgetter
 import pickle
 import constants
 
-# Exposes df_all, a dictionary with dataframes holding all global data
-#     df_all = {
-#         'cases': <all global cases dataframe>,
-#         'deaths': <all global deaths dataframe>,
-#         'recovered': <all global recoveries dataframe>
-#     }
 
-# Dataframe functions
-#   `filter(df, column, vlaue)` Generic filter
-#   `for_country(df, country)` Filter by country
-#   `for_province_state(df, province_state)` Filter by province_state
-#   `sum_by_date(df)` Group by date and sum case counts 
+""" Exposes df_all, a dictionary with dataframes holding all global data
+     df_all = {
+         'cases': <all global cases dataframe>,
+         'deaths': <all global deaths dataframe>,
+         'recovered': <all global recoveries dataframe>
+     }
 
-# Perform ETL on a Johns Hopkins COVID-19 CSV file, Returning a dataframe
+    Dataframe functions
+    `filter(df, column, vlaue)` Generic filter
+    `for_country(df, country)` Filter by country
+    `for_province_state(df, province_state)` Filter by province_state
+    `sum_by_date(df)` Group by date and sum case counts 
+"""
+
 def df_from_csv(file_name):
+    """ Perform ETL on a Johns Hopkins COVID-19 CSV file, Returning a dataframe """
     df = pd.read_csv(file_name)
     df = df.rename(columns=constants.RENAMED_COLUMNS)
     date_cols = df.filter(regex=('^\d+/\d+/\d+$')).columns.array
@@ -28,23 +30,23 @@ def df_from_csv(file_name):
     df.day = df.day.apply(lambda day: int(round(day)))
     return df[['date', 'day', 'cases', 'province_state', 'country', 'lat', 'long']]
 
-# General purpose filter.
 def filter(df, column, value):
+    """ General purpose filter. """
     return df[df[column] == value].reset_index()
 
-# Filter on country
 def for_country(df, country):
+    """ Filter on country """
     return filter(df, 'country', country)
 
-# Filter on province_state. us.py has its own function for this
 def for_province_state(df, province_state):
+    """ Filter on province_state. us.py has its own function for this """
     return filter(df, 'province_state', province_state)
 
-# Return input with all rows collapsed by date and cases summed
 def sum_by_date(df):
+    """ Return input with all rows collapsed by date and cases summed """
     return df.groupby('date').sum().reset_index()
 
-# Dictionary containing dataframes for all global data
+""" Dictionary containing dataframes for all global data """
 df_all = {
     'cases': df_from_csv(constants.DATA_URLS['cases']),
     'deaths': df_from_csv(constants.DATA_URLS['deaths']),
