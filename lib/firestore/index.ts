5ca48ab2e1c21b594b0d831c153f367cbbf1746e
Will Haslett <>
2020-03-22 11:51:31 -0400

    Added imports to index.ts

--- lib/firestore/index.ts
+++ lib/firestore/index.ts
@@ -1,25 +1,37 @@
-// https://www.npmjs.com/package/node-firestore-import-export
 import {firestoreImport} from 'node-firestore-import-export';
 import * as firebase from 'firebase-admin';
-import {FIREBASE_CREDENTIALS}  from './firebase_credentials';
+import * as fs from 'fs';
+import * as yaml from 'js-yaml';
 
 
-firebase.initializeApp({
-    apiKey: API_KEY,
-    authDomain: `${APP_NAME}.firebaseapp.com`,         
-    databaseURL: `https://${APP_NAME}.firebaseio.com`, 
-    storageBucket: `${APP_NAME}.appspot.com`,          
-});
 
-const data = {
-  "__collections__": {
-    "covid19_us_data": {
-      "us_cases": 
-    }
-  }
-};
 
-const collectionRef = firebase.firestore().collection('covid19_us_data');
 
-firestoreImport(data, collectionRef)
-    .then(()=>console.log('Data were imported.'));
+
+
+
+
+
+
+
+// https://www.npmjs.com/package/node-firestore-import-export
+
+// firebase.initializeApp({
+//     apiKey: API_KEY,
+//     authDomain: `${APP_NAME}.firebaseapp.com`,         
+//     databaseURL: `https://${APP_NAME}.firebaseio.com`, 
+//     storageBucket: `${APP_NAME}.appspot.com`,          
+// });
+
+// const data = {
+//   "__collections__": {
+//     "covid19_us_data": {
+//       "us_cases": 
+//     }
+//   }
+// };
+
+// const collectionRef = firebase.firestore().collection('covid19_us_data');
+
+// firestoreImport(data, collectionRef)
+//     .then(()=>console.log('Data were imported.'));
