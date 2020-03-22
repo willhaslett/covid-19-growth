23c098b3000b521ff977f25222f57b7e8c490db1
Will Haslett <>
2020-03-22 12:05:58 -0400

    Working on getting to STDOUT from index.ts

--- lib/firestore/index.ts
+++ lib/firestore/index.ts
@@ -3,18 +3,18 @@ import * as firebase from 'firebase-admin';
 import * as fs from 'fs';
 import * as yaml from 'js-yaml';
 
-
-
-
-
-
-
-
-
-
-
-
 // https://www.npmjs.com/package/node-firestore-import-export
+
+let credential_file = fs.readFileSync('../../.firebase_credentials.yaml', 'utf8');
+let credentials = yaml.safeLoad(credential_file);
+
+console.log(credentials)
+
+
+
+
+
+
 
 // firebase.initializeApp({
 //     apiKey: API_KEY,
