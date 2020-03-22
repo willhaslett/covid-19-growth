151f67369b515b12090115622690f86cdbb3519e
Will Haslett <>
2020-03-22 19:34:29 -0400

    Firestore work

--- lib/upload_to_firestore.py
+++ lib/upload_to_firestore.py
@@ -1,12 +1,31 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import pandas as pd
+import pickle
 
-# Use the application default credentials
+# Create database client
 cred = credentials.Certificate('.google_service_account_key.json')
-
 firebase_admin.initialize_app(cred, {
   'projectId': 'covidlocal',
 })
-
 db = firestore.client()
+
+df_type = 'cases'
+pickle_name = 'df_us_region_and_state'
+pickle_file = open(f'output/pickles/{pickle_name}''.p', 'rb')
+df = pickle.load(pickle_file)[df_type]
+pickle_file.close()
+df = df.loc[df['day'] > 57]
+
+def row_to_document(row):
+
+
+
+
+  # doc_ref = db.collection('users').document('alovelace')
+  # doc_ref.set({
+  #     'first': 'Tony',
+  #     'last': 'Lovelace',
+  #     'born': 1815
+  # })
