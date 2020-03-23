498851b80b6179eaab1745fa01855a0f6c5fa4d3
Will Haslett <>
2020-03-23 09:22:53 -0400

    Added digest function for the raw data

--- lib/c19all.py
+++ lib/c19all.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from operator import itemgetter
+import hashlib
 import pickle
 import constants
 
@@ -18,7 +19,7 @@ import constants
     `sum_by_date(df)` Group by date and sum case counts 
 """
 
-def df_from_csv(file_name):
+def parse_df(file_name):
     """ Perform ETL on a Johns Hopkins COVID-19 CSV file, Returning a dataframe """
     df = pd.read_csv(file_name)
     df = df.rename(columns=constants.RENAMED_COLUMNS)
@@ -46,14 +47,25 @@ def sum_by_date(df):
     """ Return input with all rows collapsed by date and cases summed """
     return df.groupby('date').sum().reset_index()
 
-""" Dictionary containing dataframes for all global data """
-df_all = {
-    'cases': df_from_csv(constants.DATA_URLS['cases']),
-    'deaths': df_from_csv(constants.DATA_URLS['deaths']),
-    'recovered': df_from_csv(constants.DATA_URLS['recovered'])
+def get_digest(df):
+    sha = hashlib.sha256()
+    sha.update(bytes(df.to_string(), encoding='utf-8'))
+    return sha.hexdigest()
+
+# Load latest data from GitHub
+_dfs = {
+    'cases': pd.read_csv(constants.DATA_URLS['cases']),
+    'deaths': pd.read_csv(constants.DATA_URLS['deaths']),
+    'recovered': pd.read_csv(constants.DATA_URLS['recovered']),
 }
 
-# Optional pickle files
-# pickle_file = open('output/pickles/df_all.p', 'wb')
-# pickle.dump(df_all, pickle_file)
-# print('Updated pickle file df_all.p with global data')
+print(get_digest(_dfs['cases']))
+
+# Check cache
+
+# Cache is stale. Import and process new data. Update fingerprint
+# df_all = {
+#     'cases': df_from_csv(),
+#     'deaths': df_from_csv(constants.DATA_URLS['deaths']),
+#     'recovered': df_from_csv(constants.DATA_URLS['recovered'])
+# }
