5a01227d46e8314129307e5262852848c26bc7a3
Will Haslett <>
2020-03-23 13:58:26 -0400

    Comment

--- lib/tests.py
+++ lib/tests.py
@@ -3,6 +3,9 @@ import c19us
 import c19all
 import dump_csv_and_json
 
+# For now, just testing basic functionality. This is acheived by importing the c19all and c19us
+# Modules which create the output data when imported.
+
 # Check public functions
 c19all.for_country(c19all.df_all['cases'], 'France')
 c19all.for_province_state(c19all.df_all['cases'], 'British Columbia')
