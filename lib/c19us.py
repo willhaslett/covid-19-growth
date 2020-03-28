3a7ff36b260a56a83522ba452a0e619b94e8a9ab
Will Haslett <>
2020-03-27 20:11:27 -0400

    Filtering on known counties works

--- lib/c19us.py
+++ lib/c19us.py
@@ -4,40 +4,43 @@ import math
 import c19all
 import constants
 
+COUNTIES = constants.COUNTIES
+
+FIPS = COUNTIES.keys()
+
 DATE_RANGE = pd.date_range(start=constants.DAILY_START_DATE, end=pd.to_datetime('today')).tolist()
 
-def df_from_daily_report(url):
-    df = pd.read_csv(url)[['FIPS', 'Confirmed', 'Deaths', 'Recovered', 'Active']]
-    df = df.rename(columns=constants.RENAMED_COLUMNS['daily_reports'])
-    # Remove problematic records
-    df = df.loc[~df.fips.isnull()]
-    return df
-
-daily_dfs = {}
-for date in DATE_RANGE:
-    filename_date = date.strftime('%m-%d-%Y')
-    iso_8601_date = date.strftime('%Y-%m-%d') 
-    url = constants.DATA_URLS['daily'].replace('##-##-####', filename_date)
-    try:
-        response =  urllib.request.urlopen(url)
-    except urllib.error.HTTPError:
-        break
-    else:
-        daily_dfs[iso_8601_date] = df_from_daily_report(url)
-
-print(daily_dfs)
-
-
-_output_columns = [
+output_columns = [
     'date',
-    'day',
-    'fips'
+    'day', # since 2020-01-22
+    'fips',
     'region',
     'sub_region',
     'state',
     'county',
     'lat',
     'long',
-    'population',
-    'cases',
+    'confirmed',
+    'deaths',
+    'recovered',
+    'active',
 ]
+
+_df_us = pd.DataFrame(columns=output_columns)
+
+def df_from_daily_report(url):
+    df = pd.read_csv(url)[['FIPS', 'Confirmed', 'Deaths', 'Recovered', 'Active']]
+    df = df.rename(columns=constants.RENAMED_COLUMNS['daily_reports'])
+    return df.loc[df.fips.isin(FIPS)]
+
+dfs = []
+for date in DATE_RANGE:
+    url = constants.DATA_URLS['daily'].replace('##-##-####', date.strftime('%m-%d-%Y'))
+    try:
+        response =  urllib.request.urlopen(url)
+    except urllib.error.HTTPError:
+        break
+    else:
+        dfs.append(df_from_daily_report(url))
+
+print(dfs)
