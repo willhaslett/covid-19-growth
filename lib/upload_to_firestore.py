793a7ac57d0db22773d8f17c558d024158d87c8e
Will Haslett <>
2020-03-28 16:43:46 -0400

    Uploading US data to Firebase as a set of JSON strings works

--- lib/upload_to_firestore.py
+++ lib/upload_to_firestore.py
@@ -4,49 +4,44 @@ from firebase_admin import firestore
 import pandas as pd
 from c19us_combined import df_us
 
-
 # Be aware of Firestore pricing: https://firebase.google.com/docs/firestore/pricing
 # TODO:
 #   Async
-#   Incremental daily uploads 
+#   Incremental daily uploads
 
 # Create the database client
 cred = credentials.Certificate('.google_service_account_key.json')
 firebase_admin.initialize_app(cred, {
-  'projectId': 'your-firebase-project-id',
+    'projectId': 'covidlocal',
 })
 db = firestore.client()
 
-# Create Firestore documents from a dataframe
-def create_documents(df, collection):
-  records = df.to_dict('records')
-  for record in records:
-    doc = db.collection(collection).document()
-    print(u'Creating doc {}'.format(doc.id))
-    doc.set(record)
+# Turn date and fips into columns
+df_us = df_us.reset_index()
+
+# Break up df_us into json strings for each date
+df_us['date_string'] = df_us.date.apply(lambda date: date.strftime('%Y-%m-%d'))
+date_list = df_us.date_string.unique().tolist()
+json_by_date = {}
+for date in date_list:
+    df = df_us[df_us['date_string'] == date]
+    date_json = df.to_json(orient='index')
+    json_by_date[date] = date_json
+
+print(json_by_date.__class__)
+
+# Upload json string for each date as a document
+for date_string, json_string in json_by_date.items():
+  db.collection(u'us-data').document(date_string).set({'json': json_string})
 
 # Delete a collection. This does not work if there are sub-collections present
 # https://firebase.google.com/docs/firestore/solutions/delete-collections
 def delete_collection(coll_ref, batch_size=100):
     docs = coll_ref.limit(batch_size).stream()
     deleted = 0
-    for doc in docs: 
+    for doc in docs:
         print(u'Deleting doc {}'.format(doc.id))
         doc.reference.delete()
         deleted = deleted + 1
     if deleted >= batch_size:
         return delete_collection(coll_ref, batch_size)
-        
-# Turn date and fips into columns
-df_us = df_us.reset_index()
-
-# Break up df_us into dfs for each date
-dates = df_us.date.apply(lambda date: date.strftime('%Y-%m-%d')).unique().tolist()  
-print(dates)
-
-# Bulk uploads with all current data
-# To upload a subset, filter first, e.g.: df = df[df.country == 'Japan']
-# create_documents(c19all.df_all['cases'], 'global-cases')
-
-# Delete collections
-# delete_collection(db.collection('global-cases'))
