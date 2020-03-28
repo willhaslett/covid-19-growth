1deaf3ebbc7eacea33cf18fc22306d1a56532c25
Will Haslett <>
2020-03-27 20:53:47 -0400

    New US time series in place

--- lib/c19us.py
+++ lib/c19us.py
@@ -4,34 +4,45 @@ import math
 import c19all
 import constants
 
-COUNTIES = constants.COUNTIES
-
-FIPS = COUNTIES.keys()
+counties = pd.DataFrame(constants.COUNTIES)
+fips = constants.COUNTIES.keys()
 
 DATE_RANGE = pd.date_range(start=constants.DAILY_START_DATE, end=pd.to_datetime('today')).tolist()
 
+county_columns = [
+    'county',
+    'state',
+    'sub_region',
+    'region',
+    'lat',
+    'long',
+]
+
 output_columns = [
     'date',
-    'day', # since 2020-01-22
-    'fips',
-    'region',
-    'sub_region',
-    'state',
+    'day',
     'county',
+    'state',
+    'sub_region',
+    'region',
     'lat',
     'long',
     'confirmed',
     'deaths',
     'recovered',
-    'active',
+    'active'
 ]
 
-_df_us = pd.DataFrame(columns=output_columns)
-
-def df_from_daily_report(url):
+def df_from_daily_report(date, url):
     df = pd.read_csv(url)[['FIPS', 'Confirmed', 'Deaths', 'Recovered', 'Active']]
     df = df.rename(columns=constants.RENAMED_COLUMNS['daily_reports'])
-    return df.loc[df.fips.isin(FIPS)]
+    df = df.loc[df.fips.isin(fips)]
+    df = df.astype({'fips': 'int32'})
+    df['date'] = date 
+    df['day'] = (df.date - pd.to_datetime(df.date.iloc[0])).astype('timedelta64[D]')
+    for column in county_columns:
+        df[column] = df.apply(lambda row: counties.loc[column, str(row['fips'])], axis=1)
+    return df[output_columns]
 
 dfs = []
 for date in DATE_RANGE:
@@ -41,6 +52,8 @@ for date in DATE_RANGE:
     except urllib.error.HTTPError:
         break
     else:
-        dfs.append(df_from_daily_report(url))
+        dfs.append(df_from_daily_report(date, url))
 
-print(dfs)
+df_us = pd.concat(dfs)
+
+print(df_us)
