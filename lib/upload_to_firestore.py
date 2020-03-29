c6e2cf7339ac5c1d7639e298c5ae82181484dcb1
Will Haslett <>
2020-03-29 10:30:22 -0400

    Firestore upload noow more space-efficient

--- lib/upload_to_firestore.py
+++ lib/upload_to_firestore.py
@@ -2,7 +2,9 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 import pandas as pd
+import json
 from c19us_combined import df_us
+from constants import COUNTIES as counties
 
 # Be aware of Firestore pricing: https://firebase.google.com/docs/firestore/pricing
 # TODO:
@@ -16,8 +18,14 @@ firebase_admin.initialize_app(cred, {
 })
 db = firestore.client()
 
-# Turn date and fips into columns
+# Create a Firestore document that holds demographic information for US counties
+db.collection(
+    u'us-demographics').document(u'us-counties').set({'json': json.dumps(counties)})
+
+# Keep only the needed columns. County info is in the demographics document
 df_us = df_us.reset_index()
+df_us = df_us[['date', 'fips', 'cases_nyt', 'deaths_nyt',
+               'cases_jhu', 'deaths_jhu', 'recovered', 'active']]
 
 # Create a Firestore document that holds the column names, to accompany values documents
 df_us['date_string'] = df_us.date.apply(lambda date: date.strftime('%Y-%m-%d'))
@@ -25,7 +33,9 @@ df_us = df_us.drop(columns='date')
 columns = {}
 for i in range(0, len(df_us.columns)):
     columns[str(i)] = df_us.columns[i]
-db.collection(u'us-columns').document(u'us-combined').set(columns)
+firestore_columns = {key:val for key, val in columns.items() if val != 'date_string'}
+db.collection(u'us-columns').document(u'us-combined').set(firestore_columns)
+exit()
 
 
 # Break up df_us into json strings for each date
@@ -33,12 +43,15 @@ date_list = df_us.date_string.unique().tolist()
 json_by_date = {}
 for date in date_list:
     df = df_us[df_us['date_string'] == date]
+    df = df.drop(columns='date_string')
     date_json = df.to_json(orient='values')
     json_by_date[date] = date_json
 
 # Upload JSON string for each date as a document
 for date_string, json_string in json_by_date.items():
     db.collection(u'us-data').document(date_string).set({'json': json_string})
+
+print('Latest US data uploaded to Firestore')
 
 
 def delete_collection(coll_ref, batch_size=100):
